f79a6211b8ae98789e0bd6f43c79e1e69ee78803
jyotialpha <jyotiranj@ajatus.co.in>
2025-09-07 15:12:08 +0530

    change cross origin for deployee

--- backend-student-course-platform/server.js
+++ backend-student-course-platform/server.js
@@ -36,10 +36,19 @@ app.use((req, res, next) => {
 
 // Middleware
 app.use(helmet());
+
+//local development
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+//   credentials: true
+// }));
+
+//production
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: true,
   credentials: true
 }));
+
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
 
