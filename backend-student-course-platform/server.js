55aedc80df2bcbee4b660a94f8c5df243249aff6
jyotialpha <jyotiranj@ajatus.co.in>
2025-10-22 23:45:18 +0530

    fix the uploard json limit in server js

--- backend-student-course-platform/server.js
+++ backend-student-course-platform/server.js
@@ -56,7 +56,8 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
 // Session configuration
